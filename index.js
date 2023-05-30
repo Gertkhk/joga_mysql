803fcc153f2389c1528983ffe7255334d577bada
Gertkhk <gert.sarv@khk.ee>
2023-05-30 13:01:32 +0300

    Created article controller and route, refactored application code

--- index.js
+++ index.js
@@ -35,32 +35,12 @@ con.connect(function(err) {
     console.log("Connected to joga_mysql db");
 })
 
-// show all articles - index page
-app.get('/', (req, res) => {
-    let query = "SELECT * FROM article";
-    let articles = []
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        articles = result
-        res.render('index', {
-            articles: articles
-        })
-    })
-});
+const articleRoutes = require('./routes/article'); // import article route
 
-//show article by this slug
-app.get('/article/:slug', (req, res) => {
-    let query = `SELECT article.*, author.name AS author_name FROM article INNER JOIN author ON article.author_id = author.id WHERE article.slug="${req.params.slug}"`;
-    let article
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        article = result
-        console.log(article)
-        res.render('article', {
-            article: article
-        })
-    });
-});
+// to use article routes
+app.use('/', articleRoutes);
+app.use('/article', articleRoutes)
+
 
 app.get('/author/:id', (req, res) => {
     const authorId = req.params.id;
